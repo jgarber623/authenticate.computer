4c9c02cfc5707feaa29690b24d0cf4481ec3142d
Jason Garber <jason@sixtwothree.org>
2019-07-26 14:43:24 -0400

    Limit OmniAuth provider URLs to POST requests
    Addresses CVE-2015-9284. See omniauth/omniauth#809 for additional
    details.
    
    - https://github.com/omniauth/omniauth/pull/809#issuecomment-502079405
    - https://github.com/omniauth/omniauth/pull/963
    - https://github.com/omniauth/omniauth/wiki/Resolving-CVE-2015-9284
    - https://gist.github.com/abrom/effe58b27a4f4ac1b97fb85593d7c3be

--- lib/authenticate_computer/app.rb
+++ lib/authenticate_computer/app.rb
@@ -16,6 +16,8 @@ module AuthenticateComputer
       use OmniAuth::Builder do
         provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'read:user'
       end
+
+      OmniAuth.config.allowed_request_methods = [:post]
     end
 
     helpers do
