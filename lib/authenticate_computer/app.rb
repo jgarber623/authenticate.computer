c00c10760c78f46f8a368c43714220bd52dad339
Jason Garber <jason@sixtwothree.org>
2019-07-30 21:14:05 -0400

    Add token endpoint code and view logic

--- lib/authenticate_computer/app.rb
+++ lib/authenticate_computer/app.rb
@@ -16,7 +16,7 @@ module AuthenticateComputer
       use Rack::Session::Cookie, expire_after: 60, key: ENV['COOKIE_NAME'], secret: ENV['COOKIE_SECRET']
 
       use Rack::Protection, use: [:cookie_tossing]
-      use Rack::Protection::AuthenticityToken, allow_if: ->(env) { env['PATH_INFO'] == '/auth' && env['REQUEST_METHOD'] == 'POST' }
+      use Rack::Protection::AuthenticityToken, allow_if: ->(env) { env['REQUEST_METHOD'] == 'POST' && ['/auth', '/token'].include?(env['PATH_INFO']) }
       # use Rack::Protection::ContentSecurityPolicy
       # use Rack::Protection::StrictTransport, max_age: 31536000, include_subdomains: true, preload: true
 
@@ -115,13 +115,60 @@ module AuthenticateComputer
 
       raise HttpBadRequest, 'Authorization code verification could not be completed' unless settings.datastore.exists(key)
 
-      value = JSON.parse(settings.datastore.get(key))
+      value = JSON.parse(settings.datastore.get(key), symbolize_names: true)
 
-      settings.datastore.del(key) if value['response_type'] == 'id'
+      settings.datastore.del(key) if value[:response_type] == 'id'
 
-      json me: value['me']
+      data = { me: value[:me] }
+      data.merge(scope: value[:scope]) if value[:response_type] == 'code'
+
+      json data
     rescue Sinatra::Param::InvalidParameterError => exception
       raise HttpBadRequest, exception
+    end
+
+    # Token Request
+    # https://indieauth.spec.indieweb.org/#token-request
+    post '/token', provides: :json do
+      param :grant_type,   required: true, match: 'authorization_code'
+      param :code,         required: true, format: /^[a-f0-9]{64}$/
+      param :client_id,    required: true, format: uri_regexp
+      param :redirect_uri, required: true, format: uri_regexp
+      param :me,           required: true, format: uri_regexp
+
+      authorization_endpoint = IndieWeb::Endpoints.get(params[:me]).authorization_endpoint
+      verification_response = HTTP.headers(accept: 'application/json').post(authorization_endpoint, form: params.slice(:code, :client_id, :redirect_uri))
+
+      raise HttpBadRequest, 'Authorization code verification could not be completed' unless verification_response.status.success?
+
+      verification_json = JSON.parse(verification_response.body, symbolize_names: true)
+
+      raise HttpBadRequest, %(The requested scope is invalid) if verification_json[:scope].blank?
+
+      token = SecureRandom.hex(64)
+      value = { me: verification_json[:me], client_id: params[:client_id], scope: verification_json[:scope] }.to_json
+
+      # Expire token after 30 days
+      settings.datastore.set(token, value, ex: 2_592_000)
+
+      json access_token: token, token_type: 'Bearer', scope: verification_json[:scope], me: verification_json[:me]
+    rescue HTTP::Error, IndieWeb::Endpoints::IndieWebEndpointsError
+      raise HttpInternalServerError, 'There was a problem fulfilling the request'
+    rescue Sinatra::Param::InvalidParameterError => exception
+      raise HttpBadRequest, exception
+    end
+
+    # Access Token Verification
+    # https://indieauth.spec.indieweb.org/#access-token-verification
+    get '/token', provides: :json do
+      token = request.env['HTTP_AUTHORIZATION']
+
+      raise HttpBadRequest, 'The request is missing a valid HTTP Authorization header' unless token.present? && token.sub!(/^Bearer\s/, '')
+      raise HttpBadRequest, 'Access token verification could not be completed' unless settings.datastore.exists(token)
+
+      settings.datastore.expire(token, 2_592_000)
+
+      settings.datastore.get(token)
     end
 
     error 400 do
