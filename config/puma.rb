0ee8bb26c89cb75706676e2a9c08a65e40f47ed8
Jason Garber <jason@sixtwothree.org>
2019-08-08 14:40:19 -0400

    Add nginx config and update existing config

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,13 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+workers 2
+threads 5, 5
+
+bind 'unix:///tmp/nginx.socket'
+
+on_worker_fork do
+  FileUtils.touch('/tmp/app-initialized')
+end
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
