5190ef7a4d657ff50ad410fd117990b701bb0a5f
Jason Garber <jason@sixtwothree.org>
2021-04-29 20:52:23 -0400

    🔥🔥🔥

--- app/services/endpoint_discovery_service.rb
+++
@@ -1,7 +0,0 @@
-class EndpointDiscoveryService
-  def get(url, endpoint)
-    IndieWeb::Endpoints.get(url)[endpoint]
-  rescue IndieWeb::Endpoints::IndieWebEndpointsError
-    raise HttpInternalServerError, 'There was a problem fulfilling the request'
-  end
-end
--- spec/requests/get_homepage_spec.rb
+++
@@ -1,7 +0,0 @@
-RSpec.describe AuthenticateComputer, 'GET /' do
-  it 'renders the homepage view' do
-    get '/'
-
-    expect(last_response.status).to eq(200)
-  end
-end
