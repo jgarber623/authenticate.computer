0431884bb71d9293178e83d0e1d7fffef730cfea
Jason Garber <jason@sixtwothree.org>
2021-05-12 23:08:38 -0400

    Implement RuboCop recommendations

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
+  # rubocop:disable Style/ConditionalAssignment
   if !Rails.env.development? || ENV['LOGRAGE_IN_DEVELOPMENT'] == 'true'
     config.lograge.enabled = true
   else
     config.lograge.enabled = false
   end
+  # rubocop:enable Style/ConditionalAssignment
 end
--- lib/rails_ext/active_record_timestamps_uses_timestamp_with_time_zone.rb
+++ lib/rails_ext/active_record_timestamps_uses_timestamp_with_time_zone.rb
@@ -1,4 +1,4 @@
-require 'active_record/connection_adapters/postgresql_adapter.rb'
+require 'active_record/connection_adapters/postgresql_adapter'
 
 ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:datetime] = {
   name: 'timestamptz'
