20a53516fd9b2fb0114f3654c00233fd93228f34
Jason Garber <jason@sixtwothree.org>
2021-08-27 22:08:06 -0400

    Run FactoryBot linting rake task before test suite

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,9 @@
+Rails.application.load_tasks
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    Rake::Task['factory_bot:lint'].invoke
+  end
 end
