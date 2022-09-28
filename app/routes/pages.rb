ae0d5208142d396b749200ee4ebdc8868431cc4f
Jason Garber <jason@sixtwothree.org>
2022-09-27 21:37:23 -0400

    🔥☠️🔥☠️🔥

--- app/routes/pages.rb
+++
@@ -1,7 +0,0 @@
-class AuthenticateComputer < Sinatra::Base
-  get '/', provides: :html do
-    cache_control :public
-
-    erb :'pages/homepage'
-  end
-end
--- config/environment.rb
+++
@@ -1,5 +0,0 @@
-require 'bundler/setup'
-
-Bundler.require(:default, (ENV['RACK_ENV'] || 'development').to_sym)
-
-require File.expand_path('application', __dir__)
